f1ab4fd4fc98f25161a63262069427a7ee5e3545
d1041231086 <d1041231086@student.untan.ac.id>
2025-09-07 21:47:27 +0700

    3. Mencoba fixing code agar passed CI verification

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-//import React from 'react';
+import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text testID="welcome-text">Hello, React Native!</Text>
-      <Text testID="welcome-text">Nama: Dean Dota Petarra</Text>
-      <Text testID="welcome-text">NIM: D1041231086</Text>
+      <Text testID="name-text">Nama: Dean Dota Petarra</Text>
+      <Text testID="nim-text">NIM: D1041231086</Text>
     </View>
   );
 }
